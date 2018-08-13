8ff3e9373da24f2bcea3c187a6c0ae381a64f030
gaurav <gauravsaxena.cs@gmail.com>
2018-08-13 22:11:56 +0530

    model manager and refreshcodes

--- src/shortener/models.py
+++ src/shortener/models.py
@@ -2,6 +2,29 @@ from django.db import models
 
 from .utils import code_generator, create_shortcode
 
+
+class URLManager(models.Manager):
+	# URL.objects.all()
+	def all(self, *args, **kwargs):
+		qs_main = super(URLManager, self).all(*args, **kwargs)
+		qs = qs_main.filter(active=True)
+		return qs
+
+	# URL.objects.refresh_shortcodes()
+	def refresh_shortcodes(self):
+		qs = URL.objects.filter(id__gte=1)
+		# if items is not None and isinstance(items, int):
+		#     qs = qs.order_by('-id')[:items]
+		new_codes = 0
+		for q in qs:
+		    q.shortcode = create_shortcode(q)
+		    # print(q.shortcode)
+		    # print(q.id)
+		    q.save()
+		    new_codes += 1
+		return "New codes made: {i}".format(i=new_codes)
+
+
 class URL(models.Model):
 	url 		= models.CharField(max_length=220)
 	shortcode 	= models.CharField(max_length=15, unique=True, blank=True)  
@@ -10,6 +33,10 @@ class URL(models.Model):
 	updated 	= models.DateTimeField(auto_now=True)	   #everytime the model is saved
 	timestamp 	= models.DateTimeField(auto_now_add=True)  #when model was created
 	# empty_timestamp 	= models.DateTimeField(auto_now_add=False,auto_now=False)  #empty
+	active = models.BooleanField(default=True)
+
+	objects = URLManager()
+	abc = URLManager()
 
 	def __str__(self):
 		return str(self.url)
--- src/shortener/utils.py
+++ src/shortener/utils.py
@@ -6,9 +6,9 @@ def code_generator (size=6,chars=string.ascii_lowercase + string.digits):
 
 def create_shortcode(instance, size=6):
 	new_code = code_generator(size=size)
-	print(instance)                                          #aws.com
-	print(instance.__class__)								 #<class 'shortener.models.URL'>
-	print(instance.__class__.__name__)                       #URL
+	# print(instance)                                          #aws.com
+	# print(instance.__class__)	    						   #<class 'shortener.models.URL'>
+	# print(instance.__class__.__name__)                       #URL
 	Klass = instance.__class__
 	qs_exists = Klass.objects.filter(shortcode=new_code).exists()
 	if qs_exists:                                            #when shortcode already exists in db
