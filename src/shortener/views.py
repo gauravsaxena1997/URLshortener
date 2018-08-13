085d0aa7099dbd875b034332d077a1a16d0a809e
gaurav <gauravsaxena.cs@gmail.com>
2018-08-14 00:01:45 +0530

    HttpResponseDirect

--- src/shortener/views.py
+++ src/shortener/views.py
@@ -1,16 +1,9 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 
 from .models import URL
 
-
 def redirect_view (request,shortcode=None, *args, **kwargs):
-	
 	obj  = get_object_or_404( URL,shortcode = shortcode)     #(class_name, field_name)
-
-	# try:
-	# 	obj = URL.objects.get(shortcode=shortcode)
-	# except:	
-	# 	obj = URL.objects.all().first()
-
-	return HttpResponse ("{url} , {timestamp}".format(url=obj.url,timestamp=obj.timestamp))
+	# return HttpResponse ("{url}".format(url=obj.url))
+	return HttpResponseRedirect(obj.url)
