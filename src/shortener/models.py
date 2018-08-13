c9d34c7f19f3d2879b4fb5075ef5f9c533f7d402
gaurav <gauravsaxena.cs@gmail.com>
2018-08-13 20:42:29 +0530

    shell

--- src/shortener/models.py
+++ src/shortener/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
 class URL(models.Model):
-	url = models.CharField(max_length=220)
-	shortcode = models.CharField(max_length=15, unique=True)  #By Default null=False, blank=False
+	url 		= models.CharField(max_length=220)
+	shortcode 	= models.CharField(max_length=15, unique=True)  #By Default null=False, blank=False
 	# shortcode = models.CharField(max_length=15, default='shortcode')
-	
+	updated 	= models.DateTimeField(auto_now=True)	   #everytime the model is saved
+	timestamp 	= models.DateTimeField(auto_now_add=True)  #when model was created
+	# empty_timestamp 	= models.DateTimeField(auto_now_add=False,auto_now=False)  #empty
 
 	def __str__(self):
 		return str(self.url)
