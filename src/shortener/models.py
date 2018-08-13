a4d482e52921b426ef87634ae79d904ab4aab799
gaurav <gauravsaxena.cs@gmail.com>
2018-08-13 20:05:50 +0530

    Model URL

--- src/shortener/models.py
+++ src/shortener/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 class URL(models.Model):
 	url = models.CharField(max_length=220)
+	shortcode = models.CharField(max_length=15, unique=True)  #By Default null=False, blank=False
+	# shortcode = models.CharField(max_length=15, default='shortcode')
+	
 
 	def __str__(self):
 		return str(self.url)
