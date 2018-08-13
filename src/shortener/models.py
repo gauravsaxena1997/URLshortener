e31cc1376c327a360dd217d04150eac1e97bb112
gaurav <gauravsaxena.cs@gmail.com>
2018-08-13 21:38:30 +0530

    dynamic shortcode generator

--- src/shortener/models.py
+++ src/shortener/models.py
@@ -1,14 +1,11 @@
-import random
-import string
 from django.db import models
 
-
-def code_generator (size=6,chars=string.ascii_lowercase + string.digits):
-	return ''.join(random.choice(chars) for _ in range(size))
+from .utils import code_generator, create_shortcode
 
 class URL(models.Model):
 	url 		= models.CharField(max_length=220)
-	shortcode 	= models.CharField(max_length=15, unique=True)  #By Default null=False, blank=False
+	shortcode 	= models.CharField(max_length=15, unique=True, blank=True)  
+	#By Default null=False, blank=False
 	# shortcode = models.CharField(max_length=15, default='shortcode')
 	updated 	= models.DateTimeField(auto_now=True)	   #everytime the model is saved
 	timestamp 	= models.DateTimeField(auto_now_add=True)  #when model was created
@@ -18,10 +15,8 @@ class URL(models.Model):
 		return str(self.url)
 
 	def save(self, *args, **kwargs):
-		print('something')
-		self.shortcode = code_generator()
-	#       if self.shortcode is None or self.shortcode == "":
-	#           self.shortcode = create_shortcode(self)
-	#       if not "http" in self.url:
-	#           self.url = "http://" + self.url
+		if self.shortcode is None or self.shortcode == "":
+			self.shortcode = create_shortcode(self)
+		#       if not "http" in self.url:
+		#           self.url = "http://" + self.url
 		super(URL, self).save(*args, **kwargs)
