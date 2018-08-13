5e8928155bee0f5aad22b82b132cb5d77e732cd5
gaurav <gauravsaxena.cs@gmail.com>
2018-08-13 23:41:52 +0530

    query the db with the shortcode

--- src/shortener/views.py
+++ src/shortener/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
 
-# Create your views here.
+from .models import URL
+
+
+def redirect_view (request,shortcode=None, *args, **kwargs):
+	
+	obj  = get_object_or_404( URL,shortcode = shortcode)     #(class_name, field_name)
+
+	# try:
+	# 	obj = URL.objects.get(shortcode=shortcode)
+	# except:	
+	# 	obj = URL.objects.all().first()
+
+	return HttpResponse ("{url} , {timestamp}".format(url=obj.url,timestamp=obj.timestamp))
