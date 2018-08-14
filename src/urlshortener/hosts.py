902527cc00da5480082dd684f4acd5a124118bdd
gaurav <gauravsaxena.cs@gmail.com>
2018-08-14 19:04:10 +0530

    fixed untracked files2

--- src/urlshortener/hosts.py
+++
@@ -1,9 +0,0 @@
-from django.conf import settings
-from django_hosts import patterns, host
-
-from urlshortener.hostsconf import urls as redirect_urls
-
-host_patterns = patterns('',
-    host(r'www', settings.ROOT_URLCONF, name='www'),
-    host(r'(?!www).*',redirect_urls, name='wildcard'),
-)
